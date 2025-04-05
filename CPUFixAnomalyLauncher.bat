9bf613f88e6aeee2365c75f342a11396565e3d19
Samuel Oliveira <renard162@gmail.com>
2025-04-05 07:00:08 -0300

    Por motivo de falsos positivos criados pelo PyInstaller, modificado o scopo do projeto para rodar o script python por meio de um batch file (.bat) sobre um interpretador python proprio enviado junto ao projeto.

---
+++ CPUFixAnomalyLauncher.bat
@@ -0,0 +1,2 @@
+@echo off
+.\CPUFixAnomalyLauncher\python312_embed\python.exe .\CPUFixAnomalyLauncher\launcher.py %*
--- build_launcher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del .\dist\launcher.exe
-del .\dist\CPUFixAnomalyLauncher.exe
-..\venv\Scripts\pyinstaller.exe --onefile --icon=Anomaly.ico launcher.py
-ren .\dist\launcher.exe CPUFixAnomalyLauncher.exe
-pause
